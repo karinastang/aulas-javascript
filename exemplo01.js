912c1666f3aa17707b4e32d2d661f8e0bdb14530
karinastang <karina_stang@estudante.sesisenai.org.br>
2026-09-03 20:13:48 -0300

    Add files via upload

---
+++ exemplo01.js
@@ -0,0 +1,2 @@
+let dado = 'Olá'; // tipo String
+dado = 42;      // agora é Number
---
+++ exemplo02.js
@@ -0,0 +1,5 @@
+const nome = 'Maria';
+const msg = `Olá, ${nome}. Bem-vinda!`;
+console.log(msg);
+
+console.log(nome);
