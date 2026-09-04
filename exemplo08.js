234632806fe93ee298d82dc99c67b342fd80dcc8
karinastang <karina_stang@estudante.sesisenai.org.br>
2026-09-03 21:02:30 -0300

    Add files via upload
    adicionei exemplos

---
+++ exemplo08.js
@@ -0,0 +1,6 @@
+// Sintaxe: condicao ? retorno_se_verdadeiro : retorno_se_falso
+
+const idade = 20;
+const status = idade >= 18 ? 'Maior de idade' : 'Menor de idade';
+//reduz o código de 6 para 1 linha só
+console.log(status); // Imprime: 'Maior de idade'
